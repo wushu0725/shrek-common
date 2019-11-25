e643816e2910ab60e9edfc422b65cd2e32204680
wushu@gzstrong.cn <wushu@gzstrong.cn>
2019-11-25 17:30:49 +0800

    初始化 redis的springboot的starter的工具包

--- spring-boot-redis-starter/src/main/java/com/shrek/redis/starter/properties/RedisProperties.java
+++ spring-boot-redis-starter/src/main/java/com/shrek/redis/starter/properties/RedisProperties.java
@@ -5,9 +5,6 @@ import lombok.ToString;
 import org.apache.commons.pool2.impl.BaseObjectPoolConfig;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import redis.clients.jedis.JedisPool;
-
-import java.util.List;
 
 /**
  * @Auther: 吴署
@@ -21,28 +18,64 @@ import java.util.List;
 public class RedisProperties {
     public static final String PREFIX="shrek.redis";
 
+    /**
+     * redis 单点地址
+     */
     private String host = "localhost";
 
+    /**
+     * redis 集群地址
+     */
     private String cluster;
 
+    /**
+     * redis 哨兵主
+     */
     private String master;
 
+    /**
+     * redis 哨兵从节点
+     */
     private String nodes;
 
+    /**
+     * redis 端口
+     */
     private Integer port = 6379;
 
+    /**
+     * redis 数据库
+     */
     private Integer database = 0;
 
+    /**
+     * redis 密码
+     */
     private String password;
 
+    /**
+     * redis 读取超时时间
+     */
     private Long timeOut = 5000L;
 
+    /**
+     * redis 最大连接数
+     */
     private Integer maxActive = 200;
 
+    /**
+     * redis 获取连接时的最大等待毫秒数
+     */
     private Long maxWait = BaseObjectPoolConfig.DEFAULT_MAX_WAIT_MILLIS;
 
+    /**
+     * redis 最大空闲连接数
+     */
     private Integer maxIdle = GenericObjectPoolConfig.DEFAULT_MAX_IDLE;
 
+    /**
+     * redis 最小空闲连接数
+     */
     private Integer minIdle = GenericObjectPoolConfig.DEFAULT_MIN_IDLE;
 
 
